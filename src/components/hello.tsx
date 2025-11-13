718528d07c6511949e1b9b94c9df685fc05f3286
vaibhav540 <vaibhavmore75077@gmail.com>
2025-11-13 13:13:04 +0530

    changed in hello file

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Hello: FC<Props> = ({ name }) => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <Typography>Hello {name}</Typography>
+      <Typography>Hello {name} vaibhav</Typography>
     </Box>
   )
 }
